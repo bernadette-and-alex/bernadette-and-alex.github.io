a6d2e749509c33ea511de82ef6f6af9bcd50b0f5
bernadette-and-alex <bernadette-and-alex@gmx.at>
2021-09-01 18:14:37 +0200

    another table update

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,28 +1,31 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var ids = ["Santiago", "Fonsagrada", "Oviedo", "Lugo", "Porto", "Berducedo", "Campiello", "Fisterra", "Ponte-de-Lima", "Barcelos", "Melide", "Pacos"];
+
+var slideIndex = {};
+
+for (let i of ids) {
+  slideIndex[i] = 1;
+}
+showSlides();
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(s, n) {
+  slideIndex[s] += n;
+  showSlides();
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+function showSlides() {
+  for (let s of ids) {
+    var i;
+    var slides = document.getElementById(s).getElementsByClassName("mySlides");
+    if (slideIndex[s] > slides.length) { slideIndex[s] = 1 }
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+    if (slideIndex[s] < 1) { slideIndex[s] = slides.length }
+    for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
+    }
+
+    if (slides.length > 1) {
+      slides[slideIndex[s] - 1].style.display = "block";
+    }
   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-  slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
 }
